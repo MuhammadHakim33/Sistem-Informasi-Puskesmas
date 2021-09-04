9d1400e326ac741d250dec369eab89dcfb44486b
Muhammad Hakim <phakimb33@gmail.com>
2021-09-04 19:44:36 +0700

    Beranda Page - Banner Completed

---
+++ application/views/user/beranda_view.php
@@ -0,0 +1,7 @@
+<main>
+    <!--Banner  -->
+    <figure class="section banner" role="banner">
+        <img src="<?= base_url('asset/img/banner.png') ?>" alt="" width="600" height="300">
+    </figure>
+    
+</main>
