c2636febec5bedf356981d0b7fa71eed8dfac198
Muhammad Hakim <phakimb33@gmail.com>
2021-09-04 20:39:40 +0700

    Beranda Page Navigation Features Completed

--- application/views/user/beranda_view.php
+++ application/views/user/beranda_view.php
@@ -1,7 +1,30 @@
 <main>
     <!--Banner  -->
-    <figure class="section banner" role="banner">
+    <figure id="banner" class="section" role="banner">
         <img src="<?= base_url('asset/img/banner.png') ?>" alt="" width="600" height="300">
     </figure>
-    
+
+    <!-- Navigation Feature -->
+    <nav id="link" class="section">
+        <div class="columns is-multiline is-mobile is-variable is-2">
+            <div class="column">
+                <a href="">
+                    <div class="box icon"><i class="ri-taxi-line ri-lg"></i></div>
+                    <p class="text-body-2">Panggil Ambulan</p>
+                </a>
+            </div>
+            <div class="column">
+                <a href="">
+                    <div class="box icon"><i class="ri-message-2-line ri-lg"></i></i></div>
+                    <p class="text-body-2">Konsultasi Gratis</p>
+                </a>
+            </div>
+            <div class="column">
+                <a href="">
+                    <div class="box icon"><i class="ri-dossier-line ri-lg"></i></div>
+                    <p class="text-body-2">Daftar Layanan</p>
+                </a>
+            </div>
+        </div>
+    </nav>
 </main>
