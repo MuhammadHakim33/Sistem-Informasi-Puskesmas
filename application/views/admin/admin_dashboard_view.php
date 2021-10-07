42b8d857df5de2db67b843e134630a6b4be56709
Muhammad Hakim <phakimb33@gmail.com>
2021-10-07 18:29:45 +0700

    Admin Header - Revisi 1

--- application/views/admin/admin_dashboard_view.php
+++ application/views/admin/admin_dashboard_view.php
@@ -1,39 +1,3 @@
-<nav class="navbar is-primary" role="navigation" aria-label="main navigation">
-	<div class="container">
-		<div class="navbar-brand">
-			<a class="navbar-item" href="<?= base_url('admin') ?>"><strong>ADMIN</strong></a>
-
-			<a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false"
-				data-target="navbarBasicExample">
-				<span aria-hidden="true"></span>
-				<span aria-hidden="true"></span>
-				<span aria-hidden="true"></span>
-			</a>
-		</div>
-
-		<div id="navbarBasicExample" class="navbar-menu">
-			<div class="navbar-start">
-				<a class="navbar-item">Dashboard</a>
-				<a class="navbar-item">Pasien</a>
-				<a class="navbar-item">Dokter</a>
-				<a class="navbar-item">Kontak</a>
-			</div>
-
-			<div class="navbar-end">
-				<div class="navbar-item">
-					<div class="buttons are-small">
-						<a class="button is-danger is-light">
-							<span class="icon">
-								<i class="ri-logout-box-r-line"></i>
-							</span>
-							<span>Logout</span>
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</nav>
 
 <section class="p-5">
 	<div class="container is-flex is-align-items-center is-justify-content-space-between">
--- application/views/templates/admin_header_view.php
+++ application/views/templates/admin_header_view.php
@@ -10,3 +10,40 @@
     <title><?= $title ?></title>
 </head>
 <body>
+
+<nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+	<div class="container">
+		<div class="navbar-brand">
+			<a class="navbar-item" href="<?= base_url('admin') ?>"><strong>ADMIN</strong></a>
+
+			<a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false"
+				data-target="navbarBasicExample">
+				<span aria-hidden="true"></span>
+				<span aria-hidden="true"></span>
+				<span aria-hidden="true"></span>
+			</a>
+		</div>
+
+		<div id="navbarBasicExample" class="navbar-menu">
+			<div class="navbar-start">
+				<a class="navbar-item" href="<?= base_url('admin') ?>">Dashboard</a>
+				<a class="navbar-item">Pasien</a>
+				<a class="navbar-item">Dokter</a>
+				<a class="navbar-item">Kontak</a>
+			</div>
+
+			<div class="navbar-end">
+				<div class="navbar-item">
+					<div class="buttons are-small">
+						<a class="button is-danger is-light">
+							<span class="icon">
+								<i class="ri-logout-box-r-line"></i>
+							</span>
+							<span>Logout</span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</nav>
